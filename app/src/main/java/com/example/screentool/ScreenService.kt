d9ddcf1f8566abfff685031633ce59feaa27ce18
unisocsu <rmk0557603971@outlook.com>
2026-06-24 22:44:03 +0300

    Update ScreenService.kt

--- app/src/main/java/com/example/screentool/ScreenService.kt
+++ app/src/main/java/com/example/screentool/ScreenService.kt
@@ -3,24 +3,15 @@ package com.example.screentool
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.widget.Toast
 
-class FloatingBubbleService : Service() {
+class ScreenService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Toast.makeText(this, "Floating Tool Started!", Toast.LENGTH_SHORT).show()
-        
-        // כאן יבוא קוד הבועה הצפה והנראות שלה על המסך בהמשך
-        
+        // כאן ירוץ בעתיד קוד צילום המסך ברקע
         return START_STICKY
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this, "Floating Tool Stopped", Toast.LENGTH_SHORT).show()
     }
 }
